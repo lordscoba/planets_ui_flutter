30b4ff0a0c3bf013ce6573e9bec9596044b2e455
lordscoba <lordscoba2tm@gmail.com>
2023-04-20 19:02:48 -0700

    added spalsh screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,81 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Placeholder(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/cosmic_1.png"),
+                fit: BoxFit.cover),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Logo(),
+              Flutter(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Logo extends StatelessWidget {
+  const Logo({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: const [
+        Positioned(
+          child: Image(
+            image: AssetImage("assets/images/ellipse_3.png"),
+          ),
+        ),
+        Positioned(
+          child: Image(
+            image: AssetImage("assets/images/ellipse_2.png"),
+          ),
+        ),
+        Positioned(
+          child: Image(
+            image: AssetImage("assets/images/shad.png"),
+          ),
+        ),
+        Positioned(
+          // top: 195,
+          child: Image(
+            image: AssetImage("assets/images/ellipse_1.png"),
+          ),
+        ),
+        Positioned(
+          child: Image(
+            image: AssetImage("assets/images/cosmic_logo.png"),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Flutter extends StatelessWidget {
+  const Flutter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Image(
+      width: 400,
+      image: AssetImage("assets/images/flutter_logo.png"),
     );
   }
 }
