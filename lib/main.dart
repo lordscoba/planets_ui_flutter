35072d5134f4b9c096f43981807931e0573589db
lordscoba <lordscoba2tm@gmail.com>
2023-04-30 12:48:20 -0700

    added splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:planets/favourites.dart';
 import 'package:planets/login.dart';
@@ -17,10 +18,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Planets',
+      home: AnimatedSplashScreen(
+        duration: 3000,
+        splashIconSize: double.infinity,
+        splash: const SplashScreen(),
+        nextScreen: const NextScreen(),
+        splashTransition: SplashTransition.rotationTransition,
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
+
+class NextScreen extends StatelessWidget {
+  const NextScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'Planets',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      // home: const SplashScreen(),
       initialRoute: '/login',
       routes: {
         '/': (context) => const SplashScreen(),
